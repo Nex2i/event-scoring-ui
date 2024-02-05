7ee74f98b7e3a515b80ded8019e8d16822bd01c0
Ryan H <ryanzhutch@gmail.com>
2024-02-05 16:34:35 -0700

    Remove unused code files

--- src/routes/AuthenticatedRoutes.tsx
+++ src/routes/AuthenticatedRoutes.tsx
@@ -4,22 +4,12 @@ import { lazyImport } from '@/utils/lazyImport';
 import { AuthCheckProvider } from '@/providers/AuthProviders';
 import { logRoutes, pokemonRoutes, profileRoutes } from './RouteConstants';
 const { PokemonRoutes } = lazyImport(() => import('@/features/pokemon/PokemonRoutes'), 'PokemonRoutes');
-const { LogRoutes } = lazyImport(() => import('@/features/log/LogRoutes'), 'LogRoutes');
-const { ProfileRoutes } = lazyImport(() => import('@/features/profile/ProfileRoutes'), 'ProfileRoutes');
 
 export const AuthenticatedRoutes: RouteObject[] = [
   {
     path: '/*',
     element: <AuthCheckProvider />,
     children: [
-      {
-        path: logRoutes.base + '/*',
-        element: <LogRoutes />,
-      },
-      {
-        path: profileRoutes.base + '/*',
-        element: <ProfileRoutes />,
-      },
       {
         path: pokemonRoutes.base + '/*',
         element: <PokemonRoutes />,
