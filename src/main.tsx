111673e3aa8124c00cb0fedc1eeb1cd6ca94f678
Ryan H <ryanzhutch@gmail.com>
2024-02-14 13:25:19 -0700

    Refactor for-of loop to use const in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -18,7 +18,7 @@ if ('serviceWorker' in navigator) {
   navigator.serviceWorker
     .getRegistrations()
     .then(function (registrations) {
-      for (let registration of registrations) {
+      for (const registration of registrations) {
         registration.unregister();
       }
     })
