331fd730a1e8f795d72805498350b64876623f76
Ryan H <ryanzhutch@gmail.com>
2024-04-07 20:21:22 -0600

    Refactor navigation in PublicEventRouter

--- src/features/public_event/routes/PublicEventRouter.tsx
+++ src/features/public_event/routes/PublicEventRouter.tsx
@@ -1,25 +1,42 @@
 import { Button, Typography } from '@mui/material';
 import { FC } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import * as Styled from '../publicEvent.styles';
+import { clearAll } from '@/utils/localStorage';
 
 interface PublicEventRouterProps {}
 
+enum navigationLocation {
+  INDIVIDUAL = 'individual',
+  POOL = 'pool',
+}
+
 export const PublicEventRouter: FC<PublicEventRouterProps> = ({}) => {
   const { id } = useParams() as { id: string };
+
+  const navigate = useNavigate();
+
+  const navigateToTargets = (location: navigationLocation) => {
+    clearAll();
+
+    if (location === navigationLocation.INDIVIDUAL) {
+      navigate(`/open/event/${id}/individual`);
+    } else if (location === navigationLocation.POOL) {
+      navigate(`/open/event/${id}/pool`);
+    }
+  };
+
   return (
     <Styled.PublicEventContainer>
       <Styled.PublicEventHomeInfoContainer>
         <Typography variant="h5">Are you recording for yourself or an group?</Typography>
         <br />
         <Styled.SpreadRow>
-          <Link to={`/open/event/${id}/individual`}>
-            <Button>Individual</Button>
-          </Link>
+          <Button onClick={() => navigateToTargets(navigationLocation.INDIVIDUAL)}>
+            Individual
+          </Button>
           <br />
-          <Link to={`/open/event/${id}/pool`}>
-            <Button>Group</Button>
-          </Link>
+          <Button onClick={() => navigateToTargets(navigationLocation.POOL)}>Group</Button>
         </Styled.SpreadRow>
       </Styled.PublicEventHomeInfoContainer>
     </Styled.PublicEventContainer>
