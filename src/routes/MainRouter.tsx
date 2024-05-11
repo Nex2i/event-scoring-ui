dafd2c9e947cdf7051be525bdb22bfd16b7b8488
Ryan H <ryanzhutch@gmail.com>
2024-05-10 20:15:38 -0600

    chore: Update build scripts to include setting build environment

--- src/routes/MainRouter.tsx
+++ src/routes/MainRouter.tsx
@@ -5,8 +5,17 @@ import { snackBarSelector } from '@/stores/slices/SnackBar.slice';
 import { AuthenticatedRoutes } from './AuthenticatedRoutes';
 import { PublicRoutes } from './PublicRoutes';
 
+const stickyBottomStyle: React.CSSProperties = {
+  position: 'fixed', // Positioning context fixed relative to the viewport
+  bottom: '10px', // Align to the bottom of the viewport
+  left: '10px', // Align to the left of the viewport
+  zIndex: 1000, // Ensures it stays on top of other content
+  backgroundColor: 'transparent', // Background color
+  textAlign: 'center', // Center the text inside the element
+  fontSize: '8px',
+};
+
 export const MainRouter = () => {
-  console.log('ENV VAR: ', import.meta.env);
   const snackbarProps = snackBarSelector();
 
   const element = useRoutes([...AuthenticatedRoutes, ...PublicRoutes]);
@@ -17,7 +26,7 @@ export const MainRouter = () => {
   return (
     <>
       {element}
-      <code style={{ fontSize: '8px' }}>DEV Build: {import.meta.env.VITE_COMMIT_HASH}</code>
+      <code style={stickyBottomStyle}>Build:{import.meta.env.VITE_COMMIT_HASH?.slice(0, 7)}</code>
       <SnackBarComponent
         open={snackbarProps.open}
         message={snackbarProps.message}
